f938c3a105879827fd190ea88c46a44191410309
migxel23cl <mi.pereirap@duocuc.cl>
2024-09-12 21:12:02 -0300

    se añadio animacion de carga
    123123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MatProgressSpinnerModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,11 +7,26 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  /* Estado de carga */
+  spinner = false;
 
   constructor(private router: Router) {}
 
-  goToLogin() {
-    this.router.navigate(['/login']);
+  cambiarSpinner() {
+    this.spinner = !this.spinner;
+  }
+
+  iniciarSesion() {
+    this.cambiarSpinner();
+
+    /* setTimeout permite un pequeño delay para realizar la acción */
+    setTimeout(() => {
+      // Aquí navegas a la siguiente página
+      this.router.navigate(['/login']);
+
+      // Apaga el spinner después de la navegación
+      this.cambiarSpinner();
+    }, 3000); // 3 segundos de espera antes de navegar
   }
 }
 
