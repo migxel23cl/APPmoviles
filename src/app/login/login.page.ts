a372a28f377266d42dce968f0af7e75f7455319c
migxel23cl <mi.pereirap@duocuc.cl>
2024-09-12 22:07:07 -0300

    entrega?
    1231

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,17 +9,25 @@ import { Router } from '@angular/router';
 export class LoginPage {
   usuario: string = '';
   contrasena: string = '';
+  errorMessage: string = '';
 
   constructor(private router: Router) {}
 
   iniciarSesion() {
-  
+    // Expresión regular para validar correo electrónico
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Verificar si el usuario ha ingresado un correo electrónico válido
+    if (!emailPattern.test(this.usuario)) {
+      this.errorMessage = 'Por favor, ingrese un correo electrónico válido.';
+      return; // Salir del método si la validación falla
+    }
+
     localStorage.setItem('usuario', this.usuario);
 
     console.log('Usuario:', this.usuario);
     console.log('Contraseña:', this.contrasena);
 
- 
     this.router.navigate(['/director']);
   }
 }
