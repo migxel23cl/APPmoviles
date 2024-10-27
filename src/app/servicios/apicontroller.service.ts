742e05fc0d2202aee5c754101c25259b96caff14
Migxel23cl <mi.pereirap@duocuc.cl>
2024-10-26 21:06:09 -0300

    json server descargado
    123

--- src/app/servicios/apicontroller.service.ts
+++ src/app/servicios/apicontroller.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApicontrollerService {
+export class APIControllerService {
 
-  constructor() { }
+  // URL base de la API
+  apiURL = "http://localhost:3000";
+
+  
+  constructor(private http: HttpClient) { }
+
+
+  getUsuarios(): Observable<any> {
+    return this.http.get(`${this.apiURL}/adminuser`);
+  }
+
+
+  postUsuarios(data: any): Observable<any> {
+    return this.http.post(`${this.apiURL}/adminuser`, data);
+  }
+
+
+  updateUsuarios(id: string, data: any): Observable<any> {
+    return this.http.put(`${this.apiURL}/adminuser/${id}`, data);
+  }
+
+
+  deleteUsuarios(id: string): Observable<any> {
+    return this.http.delete(`${this.apiURL}/adminuser/${id}`);
+  }
 }
+
+
